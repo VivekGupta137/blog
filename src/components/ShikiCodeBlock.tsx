013dd8f619af6118c3088146ae4c8ece3392e0d6
vivek gupta <vivekgpt107@gmail.com>
2025-08-26 02:18:53 +0530

    enhanced the stylings

--- src/components/ShikiCodeBlock.tsx
+++ src/components/ShikiCodeBlock.tsx
@@ -16,7 +16,7 @@ const ShikiCodeBlock = async (props: Props) => {
 
     const out = await codeToHtml(props.children, {
         lang: props.lang,
-        theme: "vitesse-light",
+        theme: "github-dark-dimmed",
         transformers: [
             {
                 pre(node) {
@@ -27,14 +27,14 @@ const ShikiCodeBlock = async (props: Props) => {
                     if (lineNumbers.includes(line))
                         this.addClassToHast(
                             node,
-                            "highlighted pr-4 pl-[14px] duration-300 -mx-4 inline-block w-fit min-w-[calc(100%+32px)] bg-gray-50 transition-all group-hover:bg-gray-100 dark:bg-green-900/20 border-l-2 border-l-green-500 box-border"
+                            "highlighted pr-4 pl-[12px] border-l-4 border-l-green-500 box-border duration-300 -mx-4 inline-block w-fit min-w-[calc(100%+32px)] bg-gray-50/20 transition-all group-hover:bg-gray-100/20 "
                         );
                     else
                         this.addClassToHast(
                             node,
                             cn(
                                 lineNumbers.length > 0 &&
-                                    "not-highlighted group-hover:opacity-70 transition-all duration-300"
+                                    "not-highlighted group-hover:opacity-80 transition-all duration-300"
                             )
                         );
                 },
@@ -43,15 +43,15 @@ const ShikiCodeBlock = async (props: Props) => {
     });
 
     return (
-        <div className="relative">
+        <div className="relative group">
             {props.filename && (
-                <div className="absolute -top-3 border font-mono font-bold left-0 bg-gray-100 dark:bg-gray-800 rounded-md px-2 py-1 text-sm">
+                <div className=" absolute group-hover:underline -top-4 border border-black font-mono font-bold left-0 bg-gray-100 dark:bg-gray-800 rounded-md px-2 py-1 text-sm">
                     {props.filename}
                 </div>
             )}
 
             <div
-                className=" text-lg sm:text-xl border-4 rounded-md border-dashed sm:-mx-6"
+                className=" text-lg sm:text-xl rounded-md border-black border-dashed sm:-mx-6"
                 dangerouslySetInnerHTML={{ __html: out }}
             />
         </div>
